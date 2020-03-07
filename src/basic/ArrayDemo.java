85b2da840e84e5193dbeb6df585b0c5ee31d77aa
huadong <2260332898@qq.com>
2020-03-07 22:23:54 +0800

    first commit

---
+++ src/basic/ArrayDemo.java
@@ -0,0 +1,15 @@
+package basic;
+
+import org.junit.Test;
+
+/**
+ * @author HuaZhiLong
+ * @date 2020/3/7 22:16
+ */
+public class ArrayDemo {
+
+    @Test
+    public void demo() {
+        
+    }
+}
